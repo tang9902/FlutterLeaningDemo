7bfe6ea75fd71cfddb95571c9245d1b764b53ec6
TANGYONGLAPTOP\38499 <38499237@qq.com>
2021-02-04 18:47:19 +0800

    + App图标设置 + 打包环境设置

--- lib/Widgets/c_HandAccountDemo/views/LoginView.dart
+++ lib/Widgets/c_HandAccountDemo/views/LoginView.dart
@@ -7,7 +7,42 @@ class LoginView extends StatelessWidget {
     return Material(
       child: SafeArea(
         child: Container(
-          child: Text('登录页面'),),
+            padding: EdgeInsets.only(top: 100,left: 100,right: 100),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.book,color: Colors.blueAccent,),
+                    Text('手帐小管家'),
+                  ],
+                ),
+                SizedBox(height: 50,),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: '用户名',
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: '密码',
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                ElevatedButton(
+                  child: Text('登录'),
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blueAccent,
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
